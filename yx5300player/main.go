36f640bd14a1bd65eea2cf2949dc78b148dd0527
Lee Witek <neomech@gmail.com>
2015-12-15 14:07:46 +0000

    testing commands

--- yx5300player/main.go
+++ yx5300player/main.go
@@ -13,4 +13,24 @@ func main() {
 
 	conn.WriteCommand(CMD_SEL_DEV, 0, DEV_TF)
 
+	conn.WriteCommand(CMD_SEL_DEV, 0, DEV_TF)
+	time.Sleep(500 * time.Millisecond)
+
+	conn.WriteCommand(CMD_QUERY_FLDR_COUNT, 0, 0)
+	time.Sleep(500 * time.Millisecond)
+
+	conn.WriteCommand(CMD_QUERY_TOT_TRACKS, 0, 0)
+	time.Sleep(500 * time.Millisecond)
+
+	for n := 1; n < 10; n++ {
+		conn.WriteCommand(CMD_QUERY_FLDR_TRACKS, 0, byte(n))
+		time.Sleep(500 * time.Millisecond)
+	}
+
+	conn.WriteCommand(CMD_PLAY_FOLDER_FILE, 5, 2)
+
+	for {
+		time.Sleep(5 * time.Second)
+		//conn.WriteCommand(CMD_QUERY_STATUS, 0, 0)
+	}
 }
